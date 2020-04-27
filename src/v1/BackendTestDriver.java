7fb9857ad9eb47d08ba7718336277fdf8d2d8840
jbowes628 <60439375+jbowes628@users.noreply.github.com>
2020-04-26 21:59:06 -0400

    Update BackendTestDriver.java

--- src/v1/BackendTestDriver.java
+++ src/v1/BackendTestDriver.java
@@ -13,10 +13,38 @@ public class BackendTestDriver {
 		fl.addFood("Burger", 150);
 		fl.addFood("Cheese Steak", 500);
 		fl.addFood("Candy", 20);
+		fl.addFood("Egg", 78);
+		fl.addFood("Bacon", 54);
+		fl.addFood("Sausage Link", 230);
+		fl.addFood("Cheerios", 100);
+		fl.addFood("Honey Nut Cheerios", 145);
+		fl.addFood("Yogurt", 200);
+		fl.addFood("Oatmeal", 158);
+		fl.addFood("Strawberry", 4);
+		fl.addFood("White Bread", 77);
+		fl.addFood("Pancake", 86);
+		fl.addFood("Banana", 105);
+		fl.addFood("Egg Whites", 17);
+		fl.addFood("Coffee Creamer", 70);
+		fl.addFood("Hash Brown", 305);
+		fl.addFood("Waffle", 100);
+		fl.addFood("Bagel", 300);
 		
 		ExerciseList el = new ExerciseList();
-		el.addAerobicExercise("Squats", 10, "00:20");
-		el.addRepExercise("Bench Press", 5, 9, 2000);
+		//aerobic exercises 
+		el.addAerobicExercise("Swimming", 150, "20:00");
+		el.addAerobicExercise("Running", 250, "20:00");
+		el.addAerobicExercise("Skiing", 100, "20:00");
+		el.addAerobicExercise("Stair Stepper" , 400, "20:00");
+		el.addAerobicExercise("Yoga", 200, "20:00");
+		el.addAerobicExercise("Biking", 150, "20:00");
+		el.addAerobicExercise("Rowing", 200, "20:00");
+		el.addAerobicExercise("Elliptical", 200, "20:00");
+		//rep exercises
+		el.addRepExercise("Circuit Workout", 3, 9, 500);
+		el.addRepExercise("Lifting Weights", 10, 5, 300);
+		el.addRepExercise("Lifting Weights", 10, 10, 600);
+		el.addRepExercise("HIIT", 5, 7, 400);
 		try {
 			FileOutputStream filee = new FileOutputStream("exerciselist.txt");
 			ObjectOutputStream outt = new ObjectOutputStream(filee);
